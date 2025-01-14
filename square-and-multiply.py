d9855783ec3dc7377f19bdc714ce952c15071b1f
Ramel Mirza <ramelmirza1@gmail.com>
2025-01-14 00:01:44 -0500

    big part of square and multiply implementation done - step 3 75% finished

--- square-and-multiply.py
+++ square-and-multiply.py
@@ -4,6 +4,7 @@ Date: 09-19-2024
 Description: This script was created for the students at Mohawk College who are taking Discrete Mathematics and Statistics from Mrs. Stojanovska-Pocuca. It acts as a guide to help aid students when studying the square and multiply algorithm.
 Version: 1.01
 """
+import math
 import tkinter as tk  # tkinter
 from tkinter import ttk  # themed tkinter
 
@@ -29,6 +30,7 @@ class GUIApplication(tk.Tk):
         self.modulo = None
         self.button = None
         self.output = None
+        self.error = None
         self.title("The Square and Multiply Algorithm Calculator")
         self.create_widgets()
         # disabling maximization
@@ -110,8 +112,16 @@ class GUIApplication(tk.Tk):
         self.button.place(x=12, y=735)
 
         # steps
-        self.output = ttk.Label(self)
-        self.output.pack()
+        steps_style = ttk.Style()
+        steps_style.configure("Sixth.TLabel", font=("Comic Sans MS", 10, "bold"))
+        self.output = ttk.Label(self,
+                                text="Output: ")  # will be updated as square and multiply computes
+        self.output.pack(anchor="center", pady=50)
+
+        # error label
+        error_style = ttk.Style()
+        error_style.configure("Seventh.TLabel", font=("Comic Sans MS", 10, "bold"))
+        self.error = ttk.Label(self, text="")
 
     def square_and_multiply(self):
         """
@@ -121,7 +131,62 @@ class GUIApplication(tk.Tk):
         e = int(self.exponent.get())
         m = int(self.modulo.get())
 
-        pass
+        ###################################
+
+        # step 1: convert exponent to binary
+        binary_str = ""
+        while e > 1:
+
+            if e % 2 == 0:
+                binary_str += '0'
+                e = e // 2
+            else:
+                binary_str += '1'
+                e = e // 2
+
+        if e == 1:
+            binary_str += '1'
+
+        binary_str_rev = reversed(binary_str)  # only to print it in the output label
+
+        # step 1.5: find powers of 2 that sum to b (effectively just finding 1's)
+        powers_of_2_and_indices = {}
+        for digit in range(0, len(binary_str)):
+            if binary_str[digit] == '1':
+                powers_of_2_and_indices[(int(math.pow(2, int(digit))))] = digit
+
+        # mapped it to a dictionary such that I get the value and the index which also happens to be the power the base is being raised to
+
+        # now I know that 2^max_power will be the limit, I can square the numbers up until the comparison equals the math.pow() of 2 and the value max_power - which is obviously the max value of the keys of the dictionary
+        max_power = max(list(powers_of_2_and_indices.keys()))
+        list_of_values = list(powers_of_2_and_indices.values())
+
+        # step 2: solve the squares
+        steps_to_complete = len(binary_str) - 1
+        i = 1
+        list_of_completed_squares = []
+        for j in range(1, max_power + 1, int(math.pow(2, i))):
+            if i == steps_to_complete:
+                break
+            post = int(math.pow(b, 2))
+            if post >= m:
+                post = post % m
+            if i in list_of_values:
+                list_of_completed_squares.append(post)
+            i += 1
+
+        # step 3: product the values that sum to the exponent
+        list_of_products = []
+        for i in list_of_completed_squares:
+            for j in list_of_completed_squares:
+                if i * j >= m:
+                    list_of_products.append((i * j) % m)
+                else:
+                    list_of_products.append(i * j)
+
+
+
+    ###################################
 
     def exit_out(self):
         """
